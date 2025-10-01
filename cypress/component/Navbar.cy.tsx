296b8f49ade664482c59c944e3396048184df471
jobetcasquejo43 <jobetcasquejo43@gmail.com>
2025-10-01 22:05:19 +0000

    added test

---
+++ cypress/component/Navbar.cy.tsx
@@ -0,0 +1,19 @@
+import Navbar from '../../src/components/Navbar';
+
+describe('<Navbar />', () => {
+  it('renders', () => {
+    cy.mount(<Navbar />);
+    cy.contains('span', 'Paws & Hearts Shelter').should('be.visible');
+    cy.contains('a', 'Animals').should('be.visible');
+    cy.contains('a', 'Donate Now').should('be.visible');
+  });
+
+  it('toggles the mobile menu', () => {
+    cy.viewport('iphone-6');
+    cy.mount(<Navbar />);
+    cy.get('button[aria-label="Toggle menu"]').click();
+    cy.contains('a', 'Animals').should('be.visible');
+    cy.get('button[aria-label="Toggle menu"]').click();
+    cy.contains('a', 'Animals').should('not.be.visible');
+  });
+});
