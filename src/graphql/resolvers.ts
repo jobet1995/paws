74fa4d4d492348cdbe9ced946db5af410d7ed69c
jcasquejo112 <jcasquejo112@gmail.com>
2025-10-01 11:02:00 +0000

    updated resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -3,7 +3,15 @@ import { GraphQLResolveInfo } from 'graphql';
 export const resolvers = {
   Query: {
     hello: (parent: unknown, args: object, context: unknown, info: GraphQLResolveInfo) => {
-      return 'Hello, world!';
+      // You can access the parent object, arguments, context, and info about the query.
+      // For example, let's log them to the console.
+      console.log({ parent, args, context, info });
+
+      // And we can use them to construct a more dynamic response.
+      const argString = JSON.stringify(args);
+      const parentString = JSON.stringify(parent);
+      
+      return `Hello! You called the '${info.fieldName}' field. Args: ${argString}. Parent: ${parentString}.`;
     },
   },
 };
